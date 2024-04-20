e21b1c2932e963211dbef2fa37c98e59b3ead3cc
MeowCeline00 <cwang325@my.bcit.ca>
2024-04-20 10:30:30 -0700

    adding music box image and buttons and musics

--- wonderland_application/pages/music/index.js
+++ wonderland_application/pages/music/index.js
@@ -1,4 +1,7 @@
 import { useState,useEffect,useRef } from "react";
+import styles from "./Music.module.css"
+import Image from "next/image";
+
 
 export default function Music() {
     const allSongs = [
@@ -7,42 +10,42 @@ export default function Music() {
           title: "happy_wonderland",
           artist: "wonderland",
           duration: "1:58",
-          src: "../public/music/happy_wonderland.mp3",
+          src: "/music/happy_wonderland.mp3",
         },
         {
           id: 1,
           title: "Raindrops_wonderland",
           artist: "wonderland",
           duration: "2:00",
-          src: "../public/music/Raindrops_wonderland.mp3",
+          src: "/music/Raindrops_wonderland.mp3",
         },
         {
           id: 2,
           title: "Rainy_Day_wonderland",
           artist: "wonderland",
           duration: "1:58",
-          src: "../public/music/Rainy_Day_wonderland.mp3",
+          src: "/music/Rainy_Day_wonderland.mp3",
         },
         {
           id: 3,
           title: "trip_wonderland",
           artist: "wonderland",
           duration: "1:58",
-          src: "../public/music/trip_wonderland.mp3",
+          src: "/music/trip_wonderland.mp3",
         },
         {
           id: 4,
           title: "Wonderland_Adventure",
           artist: "wonderland",
           duration: "1:02",
-          src: "../public/music/Wonderland_Adventure.mp3",
+          src: "/music/Wonderland_Adventure.mp3",
         },
         {
           id: 5,
           title: "wonderland_piano",
           artist: "wonderland",
           duration: "1:04",
-          src: "../public/music/wonderland_piano.mp3",
+          src: "/music/wonderland_piano.mp3",
         },
       ];
       const [currentSongIndex, setCurrentSongIndex] = useState(0);
@@ -60,10 +63,13 @@ export default function Music() {
         if (audioRef.current) {
         audioRef.current.src = allSongs[currentSongIndex].src
         setIsPlaying(true)}
-      }, [currentSongIndex])
+      }, [currentSongIndex,isPlaying])
 
       const handlePlayPause = () => {
         setIsPlaying(!isPlaying)
+        if (!isPlaying) {
+            audioRef.current.play().catch((error) => console.error(error))
+        }
       }
 
       const handleNext = () => {
@@ -85,8 +91,11 @@ export default function Music() {
       }
 
       return (
-        <div>
-            <audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onEnded={handleNext}>
+        <div className={`${styles.container}`}>
+          <div className={`${styles.musicBoxContainer}`}>
+            <Image className={styles.mainPhoto} src="/images/alice002.png"  width={340} height={370} />
+         
+            <audio ref={audioRef} onTimeUpdate={handleTimeUpdate} onEnded={handleNext} controls={false} autoPlay={false}>
 
             </audio>
             <div>
@@ -94,16 +103,24 @@ export default function Music() {
                     Previous
                 </button>
                 <button onClick={handlePlayPause}>
-                    Pause
+                    {isPlaying?"pause": "play"}
                 </button>
                 <button onClick={handleNext}>
                     Next
                 </button>
             </div>
+            </div>
             <div>
-                <input type="range" value={currentTime} max={audioRef.current?audioRef.current.duration:0} onChange={handleSeek}>
-                
-                </input>
+            {audioRef.current && (
+        <div>
+          <input
+            type="range"
+            value={currentTime}
+            max={audioRef.current.duration}
+            onChange={handleSeek}
+          />
+        </div>
+      )}
             </div>
         </div>
       )
